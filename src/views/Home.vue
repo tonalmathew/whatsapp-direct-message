f001040333173d7b660ef6ec0c2b28973a870f89
tonalmathew <tonalmathew1999@gmail.com>
2021-05-09 21:11:02 +0530

    working, icon not working

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,82 @@
 <template>
   <ion-page>
     <ion-header :translucent="true">
-      <ion-toolbar>
-        <ion-title>Whatsapp Direct Message</ion-title>
+      <ion-toolbar color="success">
+        <ion-title><b>Whatsapp Direct Message</b></ion-title>
       </ion-toolbar>
     </ion-header>
-    
+
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
       </ion-header>
-    
-      <div id="container">
 
+      <div id="container">
+        <ion-item>
+          <!-- <ion-icon name="call" slot="start"></ion-icon> -->
+          <ion-label position="floating">Enter Phone Number</ion-label>
+          <ion-input v-model="phoneNumber" />
+        </ion-item>
+        <br />
+        <ion-item>
+          <!-- <ion-icon slot="start" name="chatbox-ellipses-outline"></ion-icon> -->
+          <ion-label position="floating">Enter Message</ion-label>
+          <ion-textarea v-model="message" />
+        </ion-item>
+        <br />
+        <ion-button color="success" @click="sendMessage">
+          <!-- <ion-icon name="send"></ion-icon> -->
+          Send
+          </ion-button>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonTextarea,
+  IonButton,
+  // IonIcon,
+} from "@ionic/vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonTextarea,
+    IonButton,
+    // IonIcon,
+  },
+  data() {
+    return {
+      phoneNumber:"",
+      message:""
+    };
+  },
+  methods: {
+    sendMessage() {
+      const url = `https://wa.me/+91${this.phoneNumber}?text=${this.message}`
+      window.location.href = url
+    }
   }
 });
 </script>
@@ -39,7 +84,7 @@ export default defineComponent({
 <style scoped>
 #container {
   text-align: center;
-  
+
   position: absolute;
   left: 0;
   right: 0;
@@ -55,13 +100,14 @@ export default defineComponent({
 #container p {
   font-size: 16px;
   line-height: 22px;
-  
+
   color: #8c8c8c;
-  
+
   margin: 0;
 }
 
 #container a {
   text-decoration: none;
 }
+
 </style>
