ad8f9d213b0eb31dfe25ec94b4a08a4761391756
tonalmathew <tonalmathew1999@gmail.com>
2021-05-10 15:20:42 +0530

    background vishayam

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,11 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
+      <!-- <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Blank</ion-title>
         </ion-toolbar>
-      </ion-header>
+      </ion-header> -->
 
       <div id="container">
         <ion-item>
